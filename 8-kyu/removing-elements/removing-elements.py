69991a3f6e43529836c182a56f2f6b6821a0b7ac
Meshqc <124761081+Meshqc@users.noreply.github.com>
2025-09-20 19:45:25 -0400

    Update solution - CodeHub

--- 8-kyu/removing-elements/removing-elements.py
+++ 8-kyu/removing-elements/removing-elements.py
@@ -1,3 +1,9 @@
 def remove_every_other(my_list):
-    return my_list[::2]
+    r = []
+    for i in range(len(my_list)):
+        if i % 2 == 0:
+            r.append(my_list[i])
+    return r
+            
+            
 ​
