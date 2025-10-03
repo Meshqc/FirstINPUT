40828f21fe7fd7547ec6817e384885e984c456eb
Meshqc <124761081+Meshqc@users.noreply.github.com>
2025-10-02 22:56:40 -0400

    Time: 5 ms (36.59%) | Memory: 19.5 MB (76.88%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,7 +1,8 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = 0
-        for n in nums:
-            res ^= n
-        return res
+        nums.sort()
+        for i in range(0, len(nums)-1, 2):
+            if nums[i] != nums[i+1]:
+                return nums[i]
+        return nums[-1]
             
