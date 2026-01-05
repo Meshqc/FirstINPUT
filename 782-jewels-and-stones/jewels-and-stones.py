4811250f7b924b728fb23149e64b328f27a47f3d
Mesh <124761081+Meshqc@users.noreply.github.com>
2026-01-04 20:38:26 -0500

    Time: 0 ms (100.00%) | Memory: 17.2 MB (95.03%) - LeetSync

--- 782-jewels-and-stones/jewels-and-stones.py
+++ 782-jewels-and-stones/jewels-and-stones.py
@@ -1,10 +1,8 @@
 class Solution:
     def numJewelsInStones(self, j: str, s: str) -> int:
-        string = j
-        dic = dict.fromkeys(string, 0)
+        j = set(j)
+        count = 0 
         for char in s:
-            if char in dic:
-                dic[char] += 1
-        print(dic)
-        s = sum(dic.values())
-        return s
+            if char in j:
+                count += 1
+        return count
