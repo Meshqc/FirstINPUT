2fcc2c54e432c6500fd798a8c08c7e3daabcdf81
Mesh <124761081+Meshqc@users.noreply.github.com>
2026-01-05 14:53:40 -0500

    Time: 3 ms (65.91%) | Memory: 17.1 MB (96.87%) - LeetSync

--- 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
+++ 3872-find-most-frequent-vowel-and-consonant/find-most-frequent-vowel-and-consonant.py
@@ -1,19 +1,12 @@
+from collections import defaultdict
 class Solution:
     def maxFreqSum(self, s: str) -> int:
-        vow = {}
-        con = {}
+        vow = defaultdict(int)
+        con = defaultdict(int)
         vowels = "aeiou"
         for char in s:
             if char in vowels:
-                if char in vow:
-                    vow[char] +=1
-                else:
-                    vow[char] = 1
+                vow[char] +=1
             else:
-                if char in con:
-                    con[char] +=1
-                else:
-                    con[char] = 1
-        print(vow)
-        print(con)
+                con[char] +=1 
         return max(vow.values(), default=0)+max(con.values(), default=0)
