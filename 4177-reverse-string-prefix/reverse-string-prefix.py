98b28387ceabed65d756e5336facb10c7daab3c7
Mesh <124761081+Meshqc@users.noreply.github.com>
2026-01-07 11:45:52 -0500

    Time: 0 ms (100.00%) | Memory: 19.5 MB (13.87%) - LeetSync

--- 4177-reverse-string-prefix/reverse-string-prefix.py
+++ 4177-reverse-string-prefix/reverse-string-prefix.py
@@ -3,7 +3,6 @@ class Solution:
         reverse_at = k
         first_half_reversed = s[:reverse_at][::-1]
         second_half = s[reverse_at:]
-        print(first_half_reversed+second_half)
         return first_half_reversed+second_half     
 
 
